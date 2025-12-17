d9edbef31c55cd0b11591541d2ada4ceca51d6f9
Ritesh Rajendra Abuj <154005359+ritesh712004@users.noreply.github.com>
2025-12-17 19:41:16 +0530

    Update backend.tf

--- ToDo_APP/backend.tf
+++ ToDo_APP/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket = "10weeksofcloudops-week-7"
+    key    = "backend/ToDo-App.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "10weeksofcloudops-dynamo"
+  }
+}
